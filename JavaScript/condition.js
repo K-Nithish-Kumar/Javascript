f287b762aceb78e1793fe69161546567285611a1
Nithish kumar k <nithish81812@gmail.com>
2025-06-19 10:32:32 +0530

    Adding Condition.js

---
+++ JavaScript/condition.js
@@ -0,0 +1,30 @@
+// using ternary operator
+let company = prompt("enter the number",);
+
+let number = (company>0) ? alert(1) :
+             (company<0) ? alert(-1):
+             (company==0) ? alert(0): alert("unusual number");
+
+
+
+if (company>0){
+    alert(1);    
+} 
+else if(company<0) {
+    alert(-1);
+}
+else if(company==0){
+    alert(0);
+}
+else {
+    alert("unusual number");
+}
+
+
+(company>0) ? alert(1) :
+(company<0) ? alert(-1):
+(company==0) ? alert(0): alert("unusual number");
+
+
+
+
