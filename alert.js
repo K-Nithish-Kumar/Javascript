faf9d0ab4055669549a8968e178012c719874832
Nithish kumar k <nithish81812@gmail.com>
2025-06-27 23:35:06 +0530

    adding alert.js

---
+++ alert.js
@@ -0,0 +1,17 @@
+//alert message
+alert('Hello,  World!');
+
+// Prompt messsage
+// prompt message have to store in a variable and declare the answer for that variable in the alert message using backtick functionality
+
+let myAge = prompt('What is your age?', 100);
+alert(`Your age is ${myAge}`);
+console.log(`your age is ${myAge}`);
+
+
+// confirm message 
+// confirm message have to stored in a variable and then you have to declare it in the alert messages its give the answer like true or false .
+
+let isBoss = confirm('You are my boss');
+alert(isBoss);
+console.log(isBoss);
