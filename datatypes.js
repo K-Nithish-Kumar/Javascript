edcee427dda759c19bde5f20074583cafaf39822
Nithish kumar k <nithish81812@gmail.com>
2025-06-30 15:52:43 +0530

    adding datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,46 @@
+// numbers implementations
+let age = 25;
+console.log(age);
+var ageOne = 25;
+console.log(ageOne);
+const ageTwo = 25;
+console.log(ageTwo);
+
+// string implementaions
+var myName = 'Nithish kumar';
+console.log(myName);
+let myNameOne = 'Nithish kumar';
+console.log(myNameOne);
+const myNameTwo = 'Nithish kumar';
+console.log(myNameTwo);
+
+// using bacticks method
+var myAge = 12;
+console.log(`Your age is ${myAge}`);
+let myAgeOne = 14;
+console.log(`Your age is ${myAgeOne}`);
+const myAgeTwo = 16;
+console.log(`Your Age is ${myAgeTwo}`);
+
+// boolean implementaions
+var isGreater = 4 > 1;
+console.log(isGreater);
+let isGreaterOne = 5 < 2;
+console.log(isGreaterOne);
+const isGreaterTwo = 5 > 2;
+console.log(isGreaterTwo);
+
+// bigint implementations
+const bigInt = 131325433653653532432542134131341313131131n;
+console.log(bigInt);
+
+// null
+let myNameNull = null;
+console.log(myNameNull);
+
+// undefined
+let mineName = undefined;
+console.log(mineName);
+
+// typeof
+console.log(typeof 'one');
