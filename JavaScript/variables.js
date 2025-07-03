39a9ff880814d677d7dcf3e54799b72f11c93ceb
Nithish kumar k <nithish81812@gmail.com>
2025-07-03 19:01:13 +0530

    Adding variable.js

---
+++ JavaScript/variables.js
@@ -0,0 +1,13 @@
+// var declarations -> it is in global scope
+var myName;
+myName = 'Nithish Kumar'
+console.log(myName);
+
+// let declarations -> it is in specific scope
+let myNameOne;
+myNameOne = 'Nithish Kumar'
+console.log(myNameOne);
+
+// const declarations -> it is in a specific scope both let and const have the specific scope!
+const myNameTwo = 'Nithish kumar';
+console.log(myNameTwo);
